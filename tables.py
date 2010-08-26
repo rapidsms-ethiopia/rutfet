b17a3bf04cbf369cd20d0cc6cc4d862c40ff91de
andualem <andualem@andualem-laptop.(none)>
2010-08-26 22:58:43 +0300

    djtable was replaced by django-table

--- tables.py
+++ tables.py
@@ -1,46 +1,58 @@
 
-import djtables as tables
+from .models import *
+
+import django_tables as tables
+
 
 
 class EntryTable(tables.Table):
     #pk = tables.Column(visible=False, sortable=False)
-    supply_place = tables.Column(name = u"OTP/Woreda Name")
-    quantity = tables.Column(name = u"Quantity Received")
-    consumption = tables.Column(name = u"Consumption")
-    balance = tables.Column(name = u"Balance")
-    rutf_reporter = tables.Column(name = u"Reported By")
+    supply_place = tables.Column(verbose_name = 'OTP/Woreda Name')
+    quantity = tables.Column(verbose_name = 'Quantity Received')
+    consumption = tables.Column(verbose_name = 'Consumption')
+    balance = tables.Column(verbose_name = 'balance')
+    rutf_reporter = tables.Column(verbose_name = 'Reported By')
 
+    
 
 class AlertTable(tables.Table):
     #pk = tables.Column(visible=False, sortable=False)
-    notice = tables.Column(name = u"Alert Message")
-    resolved = tables.Column(name = u"Is Resolved")
-    time = tables.Column(name = u"Time")
-    rutf_reporter = tables.Column(name = u"Reported By")
+    notice = tables.Column(verbose_name = u"Alert Message")
+    resolved = tables.Column(verbose_name = u"Is Resolved")
+    time = tables.Column(verbose_name = u"Time")
+    rutf_reporter = tables.Column(verbose_name = u"Reported By")
+    
+        
     
 class SupplyTable(tables.Table):
     #pk = tables.Column(visible=False, sortable=False)
-    name = tables.Column(name = u"Supply Name")
-    code = tables.Column(name = u"Supply Code")
-    unit = tables.Column(name = u"Measurment Unit")
+    name = tables.Column(verbose_name = u"Supply Name")
+    code = tables.Column(verbose_name = u"Supply Code")
+    unit = tables.Column(verbose_name = u"Measurment Unit")
 
 class RUTFReporterTable(tables.Table):
     #pk = tables.Column(visible=False, sortable=False)
-    first_name = tables.Column(name = u"Name")
-    last_name = tables.Column(name = u"Father Name")
-    phone = tables.Column(name = u"Phone Number")
-    location = tables.Column(name = u"Location")
+    first_name = tables.Column(verbose_name = "Name")
+    last_name = tables.Column(verbose_name = "Father Name")
+    phone = tables.Column(verbose_name = "Phone Number")
+    location = tables.Column(verbose_name = "Location")
     
 class HealthPostTable(tables.Table):
     #pk = tables.Column(visible=False, sortable=False)
-    name = tables.Column(name = u"Name")
-    code = tables.Column(name = u"Code")
-    type = tables.Column(name = u"Type")
-    child_number = tables.Column(name = u"Number of Child Locations")
-    parent_name = tables.Column(name = u"Parent Location Name")
+    name = tables.Column(verbose_name = 'Name')
+    code = tables.Column(verbose_name = 'Location Code')
+    type = tables.Column(verbose_name = 'Type')
+    child_number = tables.Column(verbose_name= 'Number of Child Location')
+    parent_name = tables.Column(verbose_name = 'Parent Location')
 
-#class WebUserTable(tables.Table):
-#    pass
+
+class WebUserTable(tables.Table):
+    #pk = tables.Column(visible=False, sortable=False)
+    first_name = tables.Column(verbose_name = 'First Name')
+    last_name = tables.Column(verbose_name = 'Last Name')
+    username = tables.Column(verbose_name = 'Username')
+    location = tables.Column(verbose_name = 'Place Assigned')
+    #group = tables.Column()
 
 #class SupplyPlaceTable(tables.Table):
 #    pass
